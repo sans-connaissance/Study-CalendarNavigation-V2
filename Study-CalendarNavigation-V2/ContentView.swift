036424f80e53e3fef960f7e0c56b91bdb272f378
sans-connaissance <davemalicke@gmail.com>
2022-06-08 20:09:27 -0400

    Initial Commit

---
+++ Study-CalendarNavigation-V2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Study-CalendarNavigation-V2
+//
+//  Created by David Malicke on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Study-CalendarNavigation-V2/Study_CalendarNavigation_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Study_CalendarNavigation_V2App.swift
+//  Study-CalendarNavigation-V2
+//
+//  Created by David Malicke on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Study_CalendarNavigation_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
