1045fbbe61eace7be4e4df8fd1966d0bc3d5d259
sans-connaissance <davemalicke@gmail.com>
2022-06-09 23:00:03 -0400

    ADD Animation to Button Press state changes

--- Study-CalendarNavigation-V2/TabViewView.swift
+++ Study-CalendarNavigation-V2/TabViewView.swift
@@ -12,43 +12,51 @@ struct TabViewView: View {
     private let numbers = 1...1000
     private let colors: [Color] = [.red, .blue, .green, .black]
     @State private var selectedTab = 5
+    @State private var previouslySelectedTab = 5
     
     var body: some View {
         VStack {
             HStack {
                 Button {
-                    selectedTab -= 1
+                    withAnimation {
+                        previouslySelectedTab = selectedTab
+                        selectedTab -= 1
+                    }
                 } label: {
                     Text("Minus One")
                 }
                 
                 Button {
-                    selectedTab += 1
+                    withAnimation {
+                        previouslySelectedTab = selectedTab
+                        selectedTab += 1
+                    }
                 } label: {
                     Text("Plus One")
                 }
-
+                
             }
             TabView(selection: $selectedTab) {
-                ForEach(numbers, id: \.self) { color in
-//                    ZStack {
-                      //  color
+                ForEach(numbers, id: \.self) { number in
+                    //                    ZStack {
+                    //  color
                     VStack {
-                        Text("\(color.description)")
+                        
+                        Text("\(number.description)")
                             .font(.title)
                             .foregroundColor(.black)
-                            
-//                    }
+                        //                    }
                     }
-                    .tag(color)
+                    .tag(number)
                     .frame(width: 300, height: 1000, alignment: .center)
                     .background(.blue)
                 }
-            
+                
             }
-        .tabViewStyle(.page)
-           // .tabViewStyle(.page(indexDisplayMode: .never))
-           // .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+            .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+            .tabViewStyle(.page)
+            // .tabViewStyle(.page(indexDisplayMode: .never))
+            // .indexViewStyle(.page(backgroundDisplayMode: .interactive))
             .indexViewStyle(.page(backgroundDisplayMode: .never))
         }
     }
